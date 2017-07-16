40b7cf86378d40c35a9ae19e1e33f4c1c95955d8
Stasiuk SV <blackcatkpi@gmail.com>
2017-07-16 15:30:21 +0300

    Login and Index pages

---
+++ src/main/java/com/stasiuksv/prototype/controllers/LoginController.java
@@ -0,0 +1,17 @@
+package com.stasiuksv.prototype.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/login/")
+public class LoginController 
+{
+	@RequestMapping(method = RequestMethod.GET)
+	public String showLoginPage(ModelMap model)
+	{
+		return "login";
+	}
+}
